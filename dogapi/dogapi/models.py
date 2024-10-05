f10a0dc3905fe11af357d5ac1fb13f6abc27f656
Hinrichsta <Hinrichstylera@gmail.com>
2024-10-05 16:18:54 -0500

    working on models

--- dogapi/dogapi/models.py
+++ dogapi/dogapi/models.py
@@ -1,9 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    breed = models.CharField(max_length=100)
+    breed = models.
+    gender = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    favoritefood = models.CharField(max_length=100)
+    favoritetoy = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Breed(models.Model):
+    breed_sizes = {
+        "T": "Tiny",
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large"
+    }
+
+    name = models.CharField(max_length=100)
+    size = models.CharField(max_length=1, choices=breed_sizes)
+    friendliness = models.IntegerField()
+    trainability = models.IntegerField()
+    sheddingamount = models.IntegerField()
+    exerciseneeds = models.IntegerField()
 
     def __str__(self):
         return self.name

a854fa56279e67185725e1ea3d0ed9dc42c13dbc
Hinrichsta <Hinrichstylera@gmail.com>
2024-10-06 13:40:41 -0500

    ViewSets Complete

--- labsite/dogapi/urls.py
+++ labsite/dogapi/urls.py
@@ -1,10 +1,40 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
+from rest_framework import renderers
+from .views import *
+from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+dog_list = DogViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+dog_detail = DogViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy'
+})
+
+breed_list = BreedViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+breed_detail = BreedViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy'
+})
 
 urlpatterns = [
-    path('dog/', views.DogList.as_view()),
-    path('dog/<int:pk>/', views.DogDetail.as_view()),
-    path('breed/', views.BreedList.as_view()),
-    path('breed/<int:pk>/', views.BreedDetail.as_view())
+    path('dog/', dog_list, name='DogList'),
+    path('dog/<int:pk>/', dog_detail, name='DogDetail'),
+    path('breed/', breed_list, name='BreedList'),
+    path('breed/<int:pk>/', breed_detail, name='BreedDetail'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- labsite/dogapi/views.py
+++ labsite/dogapi/views.py
@@ -1,95 +1,136 @@
 from django.shortcuts import render
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, viewsets, permissions,renderers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from dogapi.models import *
 from dogapi.serializers import *
+from django.contrib.auth import authenticate
 
-class DogList(APIView):
+def authenticate_user(request):
+    """
+    Authenticates a user using HTTP Basic Authentication.
+
+    Returns:
+        user (User): The authenticated user object, or None if authentication fails.
+        error_response (tuple): A tuple containing the HTTP status code and message if authentication fails.
+    """
+    if 'HTTP_AUTHORIZATION' in request:
+        auth = request['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2 and auth[0].lower() == 'basic':
+            import base64
+            try:
+                username, password = base64.b64decode(auth[1]).decode('utf-8').split(':')
+                user = authenticate(username=username, password=password)
+                if user is not None and user.is_authenticated:
+                    return user, None
+            except (UnicodeDecodeError, ValueError):
+                pass
+
+    # Authentication failed
+    return None, (401, 'Unauthorized', {'WWW-Authenticate': 'Basic realm="DogService"'})
+
+class DogViewSet(viewsets.ModelViewSet):
     """
     List all dogs or create a new dog
     """
-    def get(self, request, format=None):
-        query = Dog.objects.all()
-        serializer = DogSerializer(query, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request, format=None):
-        serializer = DogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class DogDetail(APIView):
-    """
-    Retrieve, Update, or Delete an instance of Dog Data
-    """
-    def get_object(self, pk):
-        try:
-            return Dog.objects.get(pk=pk)
-        except Dog.DoesNotExist:
-            raise Http404
-        
-    def get(self, request, pk, format=None):
-        query = self.get_object(pk)
-        serializer = DogSerializer(query)
-        return Response(serializer.data)
-    
-    def put(self, request, pk, format=None):
-        query = self.get_object(pk)
-        serializer = DogSerializer(query, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        query = self.get_object(pk)
-        query.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-class BreedList(APIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class BreedViewSet(viewsets.ModelViewSet):
     """
     List all breeds or create a new breed
     """
-    def get(self, request, format=None):
-        query = Breed.objects.all()
-        serializer = BreedSerializer(query, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request, format=None):
-        serializer = BreedSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-class BreedDetail(APIView):
-    """
-    Retrieve, Update, or Delete an instance of Dog Data
-    """
-    def get_object(self, pk):
-        try:
-            return Breed.objects.get(pk=pk)
-        except Breed.DoesNotExist:
-            raise Http404
-        
-    def get(self, request, pk, format=None):
-        query = self.get_object(pk)
-        serializer = BreedSerializer(query)
-        return Response(serializer.data)
-    
-    def put(self, request, pk, format=None):
-        query = self.get_object(pk)
-        serializer = BreedSerializer(query, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        query = self.get_object(pk)
-        query.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#class DogList(APIView):
+#    """
+#    List all dogs or create a new dog
+#    """
+#    def get(self, request, format=None):
+#        query = Dog.objects.all()
+#        serializer = DogSerializer(query, many=True)
+#        return Response(serializer.data, status=status.HTTP_200_OK)
+#    
+#    def post(self, request, format=None):
+#        serializer = DogSerializer(data=request.data)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#class DogDetail(APIView):
+#    """
+#    Retrieve, Update, or Delete an instance of Dog Data
+#    """
+#    def get_object(self, pk):
+#        try:
+#            return Dog.objects.get(pk=pk)
+#        except Dog.DoesNotExist:
+#            raise Http404
+#        
+#    def get(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        serializer = DogSerializer(query)
+#        return Response(serializer.data)
+#    
+#    def put(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        serializer = DogSerializer(query, data=request.data)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#    
+#    def delete(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        query.delete()
+#        return Response(status=status.HTTP_204_NO_CONTENT)
+#    
+#class BreedList(APIView):
+#    """
+#    List all breeds or create a new breed
+#    """
+#    def get(self, request, format=None):
+#        query = Breed.objects.all()
+#        serializer = BreedSerializer(query, many=True)
+#        return Response(serializer.data, status=status.HTTP_200_OK)
+#    
+#    def post(self, request, format=None):
+#        serializer = BreedSerializer(data=request.data)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#class BreedDetail(APIView):
+#    """
+#    Retrieve, Update, or Delete an instance of Dog Data
+#    """
+#    def get_object(self, pk):
+#        try:
+#            return Breed.objects.get(pk=pk)
+#        except Breed.DoesNotExist:
+#            raise Http404
+#        
+#    def get(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        serializer = BreedSerializer(query)
+#        return Response(serializer.data)
+#    
+#    def put(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        serializer = BreedSerializer(query, data=request.data)
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(serializer.data)
+#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#    
+#    def delete(self, request, pk, format=None):
+#        query = self.get_object(pk)
+#        query.delete()
+#        return Response(status=status.HTTP_204_NO_CONTENT)

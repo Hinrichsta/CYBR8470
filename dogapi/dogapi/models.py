ba52d7f99110cbd436dc98e292dce5480a0d0818
Hinrichsta <Hinrichstylera@gmail.com>
2024-10-06 11:04:37 -0500

    models made

--- dogapi/dogapi/models.py
+++ dogapi/dogapi/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Dog(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    breed = models.
-    gender = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
-    favoritefood = models.CharField(max_length=100)
-    favoritetoy = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Breed(models.Model):
     breed_sizes = {
@@ -21,12 +9,24 @@ class Breed(models.Model):
         "L": "Large"
     }
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,primary_key=True)
     size = models.CharField(max_length=1, choices=breed_sizes)
-    friendliness = models.IntegerField()
-    trainability = models.IntegerField()
-    sheddingamount = models.IntegerField()
-    exerciseneeds = models.IntegerField()
+    friendliness = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    trainability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    sheddingamount = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    exerciseneeds = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.name
+
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, null=True, blank=True)
+    gender = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    favoritefood = models.CharField(max_length=100)
+    favoritetoy = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
